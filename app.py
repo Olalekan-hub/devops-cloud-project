587e268b3959d928f5eeda982c4ed3382f8f0e48
Yusuf Olalekan <yusufolalekan15@gmail.com>
2026-09-04 02:40:42 +0100

    Add all remaining changes

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from my DevOps project! This app is running inside Flask."
+    return "Hello from my DevOps project! This app is running inside Flask." \
+    "My name is Yusuf Hammed and i am a DevSecOps Engineer"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
