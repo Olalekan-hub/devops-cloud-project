e0cdddffc1163a9574cf4e51ae238eb32b2aa08b
Yusuf Olalekan <yusufolalekan15@gmail.com>
2026-09-05 22:08:59 +0100

    Fix PEP8 formatting issues flagged by flake8

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from my DevOps project! This app is running inside Flask. My name is Yusuf Hammed and I am a Cloud Security Engineer"
+    return "Hello from my DevOps project! This app is running inside Flask."
 
 
 if __name__ == "__main__":
